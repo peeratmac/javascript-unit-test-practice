5c2a7fc28cb764e88e35d3c21d1f30c29a3fe5e1
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-07-15 23:15:15 -0600

    Start direwolf and stark unit test

---
+++ exercises/direwolf.js
@@ -0,0 +1,13 @@
+class Direwolf {
+  constructor(name, home, size) {
+    this.name = name;
+    this.home = home || 'Beyond the Wall';
+    this.size = size || 'Massive';
+    this.starksToProtect = [];
+  }
+  protect(stark) {
+    this.starksToProtect.push(stark);
+  }
+}
+
+module.exports = Direwolf;
---
+++ exercises/stark.js
@@ -0,0 +1,8 @@
+class Stark {
+  constructor(name, location) {
+    this.name = name;
+    this.location = location || 'Winterfell';
+  }
+}
+
+module.exports = Stark;
